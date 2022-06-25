c98d378410df297f2a4d96958a0031465a0bcd5e
usmannaveed13 <usmannaveed13@gmail.com>
2022-06-25 16:38:48 +0500

    added thymeleaf and make dynamic value in html

--- src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.eazybytes.eazyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"", "/","home"})
-    public String displayHomePage() {
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Usman");
         return "home.html";
     }
 }
