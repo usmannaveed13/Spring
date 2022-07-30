353d6e0c35a0f63884321134186b444e31d6c4fc
usmannaveed13 <usmannaveed13@gmail.com>
2022-07-30 13:10:29 +0500

    added holiday controller and also holiday view

--- src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class HolidayController {
@@ -14,16 +15,21 @@ public class HolidayController {
     @GetMapping("holidays")
     public String displayHolidays(Model model) {
         List<Holiday> holidays = Arrays.asList(
-                new Holiday( " Jan 1 ","New Year's Day" , Holiday.Type.FEDERAL),
+                new Holiday( " Jan 1 ","New Year's Day" , Holiday.Type.FESTIVAL),
                 new Holiday( " Oct 31 ","Hollowen" , Holiday.Type.FESTIVAL),
                 new Holiday( " Nov 24 ","Thanksgiving Day" , Holiday.Type.FESTIVAL),
                 new Holiday( " Dec 25 ","Christmas" , Holiday.Type.FESTIVAL),
-                new Holiday( " Jan 1 ","New Year's Day" , Holiday.Type.FEDERAL),
-                new Holiday( " Jan 1 ","New Year's Day" , Holiday.Type.FEDERAL),
-                new Holiday( " Jan 1 ","New Year's Day" , Holiday.Type.FEDERAL),
-                new Holiday( " Jan 1 ","New Year's Day" , Holiday.Type.FEDERAL)
+                new Holiday( " Jan 17 ","Martin Luther King Jr. Day" , Holiday.Type.FEDERAL),
+                new Holiday( " July 4 ","Independence Day" , Holiday.Type.FEDERAL),
+                new Holiday( " Sep 5 ","Labor Day" , Holiday.Type.FEDERAL),
+                new Holiday( " Nov 11 ","Veterans Day" , Holiday.Type.FEDERAL)
         );
-        // course 88 time 3.28
-        return "";
+        Holiday.Type[] types = Holiday.Type.values();
+        for (Holiday.Type type: types){
+            model.addAttribute(type.toString(),
+                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).
+                            collect(Collectors.toList())));
+        }
+        return "holidays.html";
     }
 }
