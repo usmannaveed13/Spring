c8a757e0f6f472fcd33e9daa851c622b41e13d94
usmannaveed13 <usmannaveed13@gmail.com>
2022-08-04 17:10:29 +0500

    added lombok dependency for classes

--- src/main/java/com/eazybytes/eazyschool/model/Holiday.java
+++ src/main/java/com/eazybytes/eazyschool/model/Holiday.java
@@ -1,30 +1,15 @@
 package com.eazybytes.eazyschool.model;
 
+import lombok.*;
+
+@Data
 public class Holiday {
 
     private final String day;
     private final String reason;
     private final Type type;
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
     public enum Type {
         FESTIVAL, FEDERAL
     }
 
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
 }
