c5deed60b1d75569ae56cf9f2437605ef7f4ed10
usmannaveed13 <usmannaveed13@gmail.com>
2022-08-04 18:26:49 +0500

    removed loggerfactory and geting logger object by @slf4j

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -3,8 +3,6 @@ package com.eazybytes.eazyschool.controller;
 import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 @Controller
 public class ContactController {
-    
+
     private final ContactService contactService;
 
     @Autowired
