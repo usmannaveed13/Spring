a5f57e7615ea6b8d22a02d81f73537bb1664eaca
usmannaveed13 <usmannaveed13@gmail.com>
2022-08-17 15:55:12 +0500

    added requestScope on bean and checking on every request bean is initialized

--- src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -3,14 +3,29 @@ package com.eazybytes.eazyschool.service;
 import com.eazybytes.eazyschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Slf4j
 @Service
+@RequestScope
 public class ContactService {
+
+    private int counter = 0;
+
+    public ContactService(){
+        System.out.println("Contact Service Bean initialized");
+    }
+
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved = true;
         log.info(contact.toString());
         return isSaved;
 
+    }
+    public int getCounter() {
+        return counter;
+    }
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
