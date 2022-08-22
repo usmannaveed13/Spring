69176827d3f8a3cc04ba8235d3b425146afb88a2
usmannaveed13 <usmannaveed13@gmail.com>
2022-08-22 15:58:42 +0500

    configure custom paths authentication

--- src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -10,12 +10,22 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         // Permit All Requests inside the web application
-        http.authorizeRequests().anyRequest().permitAll()
-                .and().formLogin()
-                .and().httpBasic();
+//        http.authorizeRequests().anyRequest().permitAll()
+//                .and().formLogin()
+//                .and().httpBasic();
         // Deny All Requests inside the web application
 //        http.authorizeRequests().anyRequest().denyAll()
 //                .and().formLogin()
 //                .and().httpBasic();
+
+        http.authorizeRequests()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .and().formLogin().and().httpBasic();
+
     }
 }
