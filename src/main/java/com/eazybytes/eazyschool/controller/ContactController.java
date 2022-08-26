340b1b7c945d5c554f135c98536de46cb9cb93b8
usmannaveed13 <usmannaveed13@gmail.com>
2022-08-26 15:49:11 +0500

    added jdbc dependency and insert with jdbc template into h2 database of contact detail

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -50,8 +50,9 @@ public class ContactController {
             return "contact.html";
         }
         contactService.saveMessageDetails(contact);
-        contactService.setCounter(contactService.getCounter()+1);
-        log.info("Number of times the Contact form is submitted : "+ contactService.getCounter());
+        // counter feature is needed for bean scope application and other
+//        contactService.setCounter(contactService.getCounter()+1);
+//        log.info("Number of times the Contact form is submitted : "+ contactService.getCounter());
         return "redirect:/contact";
     }
 
--- src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -1,35 +1,70 @@
 package com.eazybytes.eazyschool.service;
 
+import com.eazybytes.eazyschool.constants.EazySchoolConstants;
 import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.context.annotation.RequestScope;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.time.LocalDateTime;
+import java.util.List;
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
+
+
 @Slf4j
 @Service
 //@RequestScope
 //@SessionScope
-@ApplicationScope
+// @ApplicationScope
 public class ContactService {
 
-    private int counter = 0;
+    @Autowired
+    private ContactRepository contactRepository;
+
+   // private int counter = 0;
 
     public ContactService(){
         System.out.println("Contact Service Bean initialized");
     }
 
     public boolean saveMessageDetails(Contact contact){
-        boolean isSaved = true;
-        log.info(contact.toString());
+      //  boolean isSaved = true;
+      //  log.info(contact.toString());
+        boolean isSaved = false;
+        contact.setStatus(EazySchoolConstants.OPEN);
+        contact.setCreatedBy(EazySchoolConstants.ANONYMOUS);
+        contact.setCreatedAt(LocalDateTime.now());
+        int result = contactRepository.saveContactMsg(contact);
+        if(result>0) {
+            isSaved = true;
+        }
         return isSaved;
+    }
+//    public List<Contact> findMsgsWithOpenStatus(){
+//        List<Contact> contactMsgs = contactRepository.findMsgsWithStatus(EazySchoolConstants.OPEN);
+//        return contactMsgs;
+//    }
 
+    public boolean updateMsgStatus(int contactId, String updatedBy){
+        boolean isUpdated = false;
+        int result = contactRepository.updateMsgStatus(contactId,EazySchoolConstants.CLOSE, updatedBy);
+        if(result>0) {
+            isUpdated = true;
+        }
+        return isUpdated;
     }
-    public int getCounter() {
-        return counter;
-    }
-    public void setCounter(int counter) {
-        this.counter = counter;
-    }
+
+//    public int getCounter() {
+//        return counter;
+//    }
+//    public void setCounter(int counter) {
+//        this.counter = counter;
+//    }
 }
