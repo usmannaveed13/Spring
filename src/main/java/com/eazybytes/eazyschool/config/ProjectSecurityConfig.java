3d26842b7383866d96319a83c2cacc35765b5a16
usmannaveed13 <usmannaveed13@gmail.com>
2022-09-08 12:26:10 +0500

    implementing password hashing with BCryptPasswordEncoder

--- src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.eazybytes.eazyschool.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -56,6 +59,12 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .withUser("admin").password("54321").roles("ADMIN")
 //                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
 //    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
 }
--- src/main/resources/eazyschool_data.sql
+++ src/main/resources/eazyschool_data.sql
@@ -30,3 +30,8 @@ INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
 
 INSERT INTO `person` (`name`,`email`,`mobile_number`,`pwd`,`role_id`,`created_at`, `created_by`)
   VALUES ('Admin','admin@eazyschool.com','3443434343','admin', 1 ,CURDATE(),'DBA');
+
+DELETE FROM person where email='admin@eazyschool.com';
+
+INSERT INTO `person` (`name`,`email`,`mobile_number`,`pwd`,`role_id`,`created_at`, `created_by`)
+  VALUES ('Admin','admin@eazyschool.com','3443434343','$2a$10$XhU4UcSxDPb5G0I0fT/CZ.Lfj2VW2fkLkUP5cOEM.xM8EzyUQXaD2', 1 ,CURDATE(),'DBA');
