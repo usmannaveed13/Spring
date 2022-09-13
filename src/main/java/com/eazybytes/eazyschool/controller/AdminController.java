8e2ce06af6738435341ff340de251f42353bd816
usmannaveed13 <usmannaveed13@gmail.com>
2022-09-13 16:01:40 +0500

    added delete students feature from course section

--- src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
@@ -157,5 +157,20 @@ public class AdminController {
         return modelAndView;
     }
 
+    @GetMapping("/deleteStudentFromCourse")
+    public ModelAndView deleteStudentFromCourse(Model model, @RequestParam int personId,
+                                                HttpSession session) {
+        Courses courses = (Courses) session.getAttribute("courses");
+        Optional<Person> person = personRepository.findById(personId);
+        person.get().getCourses().remove(courses);
+        courses.getPersons().remove(person);
+        personRepository.save(person.get());
+        session.setAttribute("courses",courses);
+        ModelAndView modelAndView = new
+                ModelAndView("redirect:/admin/viewStudents?id="+courses.getCourseId());
+        return modelAndView;
+    }
+
+
 }
 
