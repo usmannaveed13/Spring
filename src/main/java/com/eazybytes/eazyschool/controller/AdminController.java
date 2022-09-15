19a83d4347dd104776ee9241ff34acd6b2980443
usmannaveed13 <usmannaveed13@gmail.com>
2022-09-15 10:14:57 +0500

    implement dynamic sorting with spring data JPA

--- src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
@@ -8,6 +8,7 @@ import com.eazybytes.eazyschool.repository.EazyClassRepository;
 import com.eazybytes.eazyschool.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -112,7 +113,9 @@ public class AdminController {
        // List<Courses> courses = coursesRepository.findAll();
        // List<Courses> courses = coursesRepository.findByOrderByName();
         // applying order by name desc
-        List<Courses> courses = coursesRepository.findByOrderByNameDesc();
+       // List<Courses> courses = coursesRepository.findByOrderByNameDesc();
+        // applying dynamic sorting
+        List<Courses> courses = coursesRepository.findAll(Sort.by("name").descending());
         ModelAndView modelAndView = new ModelAndView("courses_secure.html");
         modelAndView.addObject("courses",courses);
         modelAndView.addObject("course", new Courses());
