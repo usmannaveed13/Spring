d6de024fbf089399599bf8f8e785d3c11fc90060
usmannaveed13 <usmannaveed13@gmail.com>
2022-09-22 16:17:04 +0500

    creating a java class for with @configuration properties and inject that bean and getting properties values

--- src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -1,5 +1,6 @@
 package com.eazybytes.eazyschool.service;
 
+import com.eazybytes.eazyschool.config.EazySchoolProps;
 import com.eazybytes.eazyschool.constants.EazySchoolConstants;
 import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.repository.ContactRepository;
@@ -33,6 +34,10 @@ public class ContactService {
     @Autowired
     private ContactRepository contactRepository;
 
+    @Autowired
+    EazySchoolProps eazySchoolProps;
+
+
    // private int counter = 0;
 
     public ContactService(){
@@ -58,7 +63,13 @@ public class ContactService {
     }
 
     public Page<Contact> findMsgsWithOpenStatus(int pageNum,String sortField, String sortDir){
-        int pageSize = 5;
+       // int pageSize = 5;
+        // now getting values from props class
+        int pageSize = eazySchoolProps.getPageSize();
+        if(null!=eazySchoolProps.getContact() && null!=eazySchoolProps.getContact().get("pageSize")){
+            pageSize = Integer.parseInt(eazySchoolProps.getContact().get("pageSize").trim());
+        }
+
         Pageable pageable = PageRequest.of(pageNum - 1, pageSize,
                 sortDir.equals("asc") ? Sort.by(sortField).ascending()
                         : Sort.by(sortField).descending());
